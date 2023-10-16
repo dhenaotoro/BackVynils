9917c50b0793404836cdd1a0b4ca0b5181fda420
Daniel Henao <daniel.henao@geniussports.com>
2023-10-16 12:06:40 -0500

    Corrige la forma de desplegar app y base de datos

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,5 @@ RUN npm install --quiet
 
 COPY . .
 
-CMD ["npm", "run", "start"]
+#CMD ["npm", "run", "start"]
+ENTRYPOINT [ "sh", "./docker/entrypoint.sh" ]
--- src/app.module.ts
+++ src/app.module.ts
@@ -40,7 +40,7 @@ import { AlbumMusicianModule } from './albummusician/albummusician.module';
       port: 5432,
       username: 'postgres',
       password: 'postgres',
-      database: 'vinyls',
+      database: 'vynils',
       entities: [Album, CollectorAlbum, Band, Collector, Comment, Musician, Performer, PerformerPrize, Prize, Track,],
       dropSchema: true,
       synchronize: true,
